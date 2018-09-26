29d8e18985af12be8bece9115e1ac4b55a856892
Patrick Kim <ptrkkim@gmail.com>
2018-09-26 00:14:40 -0400

    Set up dev-screens manually. First successful build

--- App/Config/DebugConfig.js
+++ App/Config/DebugConfig.js
@@ -1,5 +1,6 @@
 export default {
   showDevScreens: __DEV__,
+
   useFixtures: false,
   ezLogin: false,
   yellowBox: __DEV__,
--- App/Containers/LaunchScreen.js
+++ App/Containers/LaunchScreen.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { ScrollView, Text, Image, View } from 'react-native'
 import DevscreensButton from '../../ignite/DevScreens/DevscreensButton.js'
 
+
+
 import { Images } from '../Themes'
 
 // Styles
@@ -23,6 +25,7 @@ export default class LaunchScreen extends Component {
               This probably isn't what your app is going to look like. Unless your designer handed you this screen and, in that case, congrats! You're ready to ship. For everyone else, this is where you'll see a live preview of your fully functioning app using Ignite.
             </Text>
           </View>
+
 
           <DevscreensButton />
         </ScrollView>
